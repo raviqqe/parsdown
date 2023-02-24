b36ead4364c270045039442c3bf30a6c95783e29
Yota Toyama <raviqqe@gmail.com>
2023-02-24 12:29:20 +0800

    Fix linting

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,5 @@
-import { Parser } from "./parser";
 import { stringInput } from "./input";
+import { Parser } from "./parser";
 
 export const parseString = <V>(parser: Parser<string, V>, input: string): V =>
   parser(stringInput(input));
