ae3e4e4350b445f8d4b631c61c467d61abe812d3
Yota Toyama <raviqqe@gmail.com>
2024-07-19 23:35:23 +0900

    Fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,17 @@
-export { default } from "@raviqqe/eslint-config";
+import configuration from "@raviqqe/eslint-config";
+
+export default [
+  ...configuration,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
